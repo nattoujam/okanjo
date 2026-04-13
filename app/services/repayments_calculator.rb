db2b65bdbf2c33422005c0b9ae3a5bc6558d5e46
nattoujam <Public.kyuuanago@gmail.com>
2026-04-13 22:26:45 +0900

    返済額計算クラスを追加

---
+++ app/services/repayments_calculator.rb
@@ -0,0 +1,57 @@
+class RepaymentsCalculator
+  def initialize(group)
+    @group = group
+  end
+
+  def call
+    balances = calculate_balances
+    calculate_repayments(balances)
+  end
+
+  private
+
+  def calculate_balances
+    balances = Hash.new(0)
+
+    @group.payments.includes(:payment_participants).each do |payment|
+      participants = payment.payment_participants
+      share = payment.amount.to_f / participants.count
+
+      balances[payment.payer_member_id] += payment.amount
+
+      participants.each do |pp|
+        balances[pp.member_id] -= share
+      end
+    end
+
+    balances
+  end
+
+  def calculate_repayments(balances)
+    # 最小取引数アルゴリズム（貪欲法）
+    repayments = []
+
+    creditors = balances.select { |_, v| v > 0.5 }.sort_by { |_, v| -v }.to_a
+    debtors   = balances.select { |_, v| v < -0.5 }.sort_by { |_, v| v }.to_a
+
+    ci = 0
+    di = 0
+
+    while ci < creditors.size && di < debtors.size
+      cid, credit = creditors[ci]
+      did, debt   = debtors[di]
+
+      amount = [ credit, -debt ].min.round
+
+      repayments << { from: did, to: cid, amount: amount } if amount > 0
+
+      creditors[ci][1] -= amount
+      debtors[di][1]   += amount
+
+      ci += 1 if creditors[ci][1] < 0.5
+      di += 1 if debtors[di][1] > -0.5
+    end
+
+    repayments
+  end
+end
